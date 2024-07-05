b03d0b0e9d1f5d42e47376cfe94b49b117697715
leefuuchang <a0962014248gg@gmail.com>
2024-07-06 05:13:24 +0800

    merge screenshot.py and detection.py, leaving rect param on

--- detection.py
+++ detection.py
@@ -1,27 +1,43 @@
+from mss import mss
+import numpy as np
+import screeninfo
 import cv2
-import sys
 import os
 
-def DetectOn(screenShot, prevWeapon, confidence):
-    img = cv2.cvtColor(screenShot, cv2.COLOR_BGR2GRAY)
 
-    img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)[1]
+def detect(rect, prevWeapon, confidence):
+    with mss() as screen: 
+        screenShot = np.array(screen.grab(rect))
 
-    silhouettes = os.path.join("ApexGunControl-LS", "apex", "silhouette")
+        img = cv2.cvtColor(screenShot, cv2.COLOR_BGR2GRAY)
 
-    def confidenceOf(weaponName):
-        nonlocal img, silhouettes
-        sil = cv2.imread(os.path.join(silhouettes, f"{weaponName}.jpg"), cv2.IMREAD_UNCHANGED)
-        result = cv2.matchTemplate(img, sil, cv2.TM_CCOEFF_NORMED)
-        return cv2.minMaxLoc(result)[1]
+        img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)[1]
 
-    if(prevWeapon and confidenceOf(prevWeapon) > confidence): return prevWeapon
+        silhouettes = os.path.join("ApexGunControl-LS", "apex", "silhouette")
 
-    for file in os.listdir(silhouettes):
-        if(not file.endswith(".jpg")): continue
-        weaponName = os.path.splitext(file)[0]
-        if(confidenceOf(weaponName) > confidence): return weaponName
+        def confidenceOf(weaponName):
+            nonlocal img, silhouettes
+            sil = cv2.imread(os.path.join(silhouettes, f"{weaponName}.jpg"), cv2.IMREAD_UNCHANGED)
+            result = cv2.matchTemplate(img, sil, cv2.TM_CCOEFF_NORMED)
+            return cv2.minMaxLoc(result)[1]
+
+        if(prevWeapon and confidenceOf(prevWeapon) > confidence): return prevWeapon
+
+        for file in os.listdir(silhouettes):
+            if(not file.endswith(".jpg")): continue
+            weaponName = os.path.splitext(file)[0]
+            if(confidenceOf(weaponName) > confidence): return weaponName
 
     return None
 
-print(DetectOn(cv2.imread(sys.argv[1]), "R-301", 0.8))
+
+monitor = [m for m in screeninfo.get_monitors() if m.is_primary][0]
+
+w = monitor.width
+h = monitor.height
+
+base = h//6
+
+rect = (w-base*3, h-base, w, h)
+
+print(detect(rect, "", 0.8))
