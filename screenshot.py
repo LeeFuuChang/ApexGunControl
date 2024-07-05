e84d5c66b5bebeb4f3a955cf86c895ed2b33cc38
leefuuchang <a0962014248gg@gmail.com>
2024-07-06 05:05:40 +0800

    faster screenshot with mss library

--- screenshot.py
+++ screenshot.py
@@ -1,23 +1,26 @@
-from PIL import ImageGrab
+from mss import mss
 import numpy as np
-import time
+import screeninfo
 import cv2
 
-time.sleep(5)
+def get_primary_monitor() -> screeninfo.Monitor:
+    """Returns the primary monitor by :class:`Monitor`."""
+    return [m for m in screeninfo.get_monitors() if m.is_primary][0]
 
-screenshot = ImageGrab.grab()
+monitor = get_primary_monitor()
 
-w = screenshot.width
-h = screenshot.height
+w = monitor.width
+h = monitor.height
 
 base = h//6
 
-img = screenshot.crop((w-base*3, h-base, w, h))
+rect = (w-base*3, h-base, w, h)
 
-img = np.array(img.convert("RGB"))
+with mss() as sct:
+    img = np.array(sct.grab(rect))
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)[1]
+    img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)[1]
 
-cv2.imwrite("screenshot.png", img)
+    cv2.imwrite("screenshot.png", img)
