c856f90c4fe73b4f22c9c0f9b31ed84039d08053
leefuuchang <a0962014248gg@gmail.com>
2024-07-06 13:27:04 +0800

    modulize  create  function

--- silhouette.py
+++ silhouette.py
@@ -2,29 +2,46 @@ import cv2
 import sys
 import os
 
-# read
-img = cv2.imread(sys.argv[1])
 
-# crop
-cx = 1536
-cy = 960
-cw = 176
-ch = 48
-img = img[cy:cy+ch,cx:cx+cw]
+def silhouettilize(img):
+    # crop region
+    w = img.shape[1]
+    h = img.shape[0]
+    b = h//6
+    img = img[h-b:h,w-b*3:w]
 
-# grayscale
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # resize region
+    defaultB = 1080 // 6
+    img = cv2.resize(img, (defaultB*3, defaultB), interpolation=cv2.INTER_NEAREST)
 
-# binarization
-img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)[1]
+    # crop weapon
+    cx = 156
+    cy = 60
+    cw = 176
+    ch = 48
+    img = img[cy:cy+ch,cx:cx+cw]
 
-# masking
-mx = [0 , 134]
-my = [45, 38 ]
-mw = [88, 42 ]
-mh = [3 , 10 ]
-for x, y, w, h in zip(mx, my, mw, mh):
-    img[y:y+h,x:x+w] = 0
+    # grayscale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# saving
-cv2.imwrite(os.path.join("ApexGunControl-LS", "apex", "silhouettes", sys.argv[1]), img)
+    # binarization
+    img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)[1]
+
+    # masking
+    img[img.shape[0]- 3:img.shape[0], 0              :88          ] = 0
+    img[img.shape[0]-10:img.shape[0], img.shape[1]-42:img.shape[1]] = 0
+
+    # output
+    return img
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        # read
+        img = cv2.imread(sys.argv[1])
+
+        # silhouettilize
+        img = silhouettilize(img)
+
+        # saving
+        cv2.imwrite(os.path.join("ApexGunControl-LS", "apex", "silhouettes", sys.argv[1]), img)
