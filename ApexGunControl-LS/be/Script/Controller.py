0d9ca28f9586ab5422c85216fbc56241b4092a62
leefuuchang <a0962014248gg@gmail.com>
2024-07-13 15:09:08 +0800

    change 'ControlIndex' in 'Script/Controller.py' to 'recoilIndex'

--- ApexGunControl-LS/be/Script/Controller.py
+++ ApexGunControl-LS/be/Script/Controller.py
@@ -14,7 +14,7 @@ ctypes.windll.shcore.SetProcessDpiAwareness(2)
 class GameController:
     thread = None
 
-    ControlIndex = 0
+    recoilIndex = 0
 
     @classmethod
     def update(cls):
@@ -26,13 +26,13 @@ class GameController:
                         keyboard.send(os.environ["KEY_SHOOTING"], do_press=False, do_release=True)
                     recoil = GameMonitor.weaponConfig.get("recoil", [[0, 0], [0, 0]])
                     if(win32api.GetAsyncKeyState(0x2) & 0x8000 > 0):
-                        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, recoil[cls.ControlIndex][0], recoil[cls.ControlIndex][1], 0, 0)
-                    cls.ControlIndex = (cls.ControlIndex+1) % len(recoil)
+                        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, recoil[cls.recoilIndex][0], recoil[cls.recoilIndex][1], 0, 0)
+                    cls.recoilIndex = (cls.recoilIndex+1) % len(recoil)
                 else:
-                    cls.ControlIndex = 0
+                    cls.recoilIndex = 0
                     keyboard.send(os.environ["KEY_SHOOTING"], do_press=False, do_release=True)
                 if(win32api.GetAsyncKeyState(0x5) & 0x8000 > 0):
                     keyboard.send(os.environ["KEY_MOVEMENT"], do_press=True, do_release=True)
                 time.sleep(0.001)
-            cls.ControlIndex = 0
+            cls.recoilIndex = 0
             keyboard.send(os.environ["KEY_SHOOTING"], do_press=False, do_release=True)
