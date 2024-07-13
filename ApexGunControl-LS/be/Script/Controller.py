a2a1dc8295a1bf8043d12ab2b5befeb68409dc69
leefuuchang <a0962014248gg@gmail.com>
2024-07-13 15:07:58 +0800

    set new environment variables ('KEY_SHOOTING', 'KEY_MOVEMENT') in 'app.py' for dynamic update

--- ApexGunControl-LS/be/Script/Controller.py
+++ ApexGunControl-LS/be/Script/Controller.py
@@ -15,27 +15,24 @@ class GameController:
     thread = None
 
     ControlIndex = 0
-    ShootingKey = 'p'
-
-    MovementKey = 'o'
 
     @classmethod
     def update(cls):
         while(not time.sleep(.5)):
             while(os.environ["USER"] and GameMonitor.isFocused and GameMonitor.inGame):
                 if(win32api.GetAsyncKeyState(0x1) & 0x8000 > 0):
-                    keyboard.send(cls.ShootingKey, do_press=True, do_release=False)
+                    keyboard.send(os.environ["KEY_SHOOTING"], do_press=True, do_release=False)
                     if(GameMonitor.weaponConfig and GameMonitor.weaponConfig.get("tap", False)):
-                        keyboard.send(cls.ShootingKey, do_press=False, do_release=True)
+                        keyboard.send(os.environ["KEY_SHOOTING"], do_press=False, do_release=True)
                     recoil = GameMonitor.weaponConfig.get("recoil", [[0, 0], [0, 0]])
                     if(win32api.GetAsyncKeyState(0x2) & 0x8000 > 0):
                         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, recoil[cls.ControlIndex][0], recoil[cls.ControlIndex][1], 0, 0)
                     cls.ControlIndex = (cls.ControlIndex+1) % len(recoil)
                 else:
                     cls.ControlIndex = 0
-                    keyboard.send(cls.ShootingKey, do_press=False, do_release=True)
+                    keyboard.send(os.environ["KEY_SHOOTING"], do_press=False, do_release=True)
                 if(win32api.GetAsyncKeyState(0x5) & 0x8000 > 0):
-                    keyboard.send(cls.MovementKey, do_press=True, do_release=True)
+                    keyboard.send(os.environ["KEY_MOVEMENT"], do_press=True, do_release=True)
                 time.sleep(0.001)
             cls.ControlIndex = 0
-            keyboard.send(cls.ShootingKey, do_press=False, do_release=True)
+            keyboard.send(os.environ["KEY_SHOOTING"], do_press=False, do_release=True)
--- ApexGunControl-LS/be/app.py
+++ ApexGunControl-LS/be/app.py
@@ -146,6 +146,9 @@ def run():
     browserWindow = WebRenderer()
     browserWindow.connect(server, server.host, server.port)
 
+    os.environ["KEY_SHOOTING"] = "f17"
+    os.environ["KEY_MOVEMENT"] = "f18"
+
     if(GameController.thread is None):
         GameController.thread = threading.Thread(target=GameController.update, daemon=True)
         GameController.thread.start()
