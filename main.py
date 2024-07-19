3d5ec7c5f0ddae76bd39aa430a7d895a4f2de76d
leefuuchang <a0962014248gg@gmail.com>
2024-07-20 03:40:36 +0800

    remote loading splash art

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ LocalStorage = getattr(getPackage("StorageManager", os.environ["STORAGE_URL"]), 
 def init():
     loader = LoadingWindow()
     loader.setIconPath("./default-loading-icon.png")
-    loader.setSplashArtPath("./default-loading-splash.png")
+    loader.setSplashArtURL(f"{os.environ['STORAGE_URL']}/LoadingSplash.png")
     def statusCallback(text, progress):
         nonlocal loader
         loader.text = text
