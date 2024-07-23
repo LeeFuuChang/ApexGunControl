b64e0ec7583268bd06808478606a303295103f6b
leefuuchang <a0962014248gg@gmail.com>
2024-07-24 04:38:55 +0800

    add .pyc compile / ignore .pyc change / add struct.xml compile

--- StorageManager.py
+++ StorageManager.py
@@ -156,3 +156,41 @@ class LocalStorage:
         progressCallback(f"[{cls.__name__}] OK", 100)
 
         return cls.structure.attrib["name"]
+
+
+
+if __name__ == "__main__" and len(sys.argv) > 1:
+    import xml.dom.minidom
+
+    target = os.path.normpath(sys.argv[1])
+    if(not os.path.exists(target)): raise FileNotFoundError()
+
+    root = ET.Element("folder")
+    root.attrib["name"] = os.path.split(target)[1]
+
+    with open(os.path.join(target, "storage.version"), "r") as f:
+        root.attrib["version"] = f.read()
+
+    excluding = {
+        "__pycache__", ".py", ".DS_Store", ".version"
+    }
+
+    def walk(root, node, path):
+        for child in sorted(os.listdir(path), key=lambda c : os.path.isdir(os.path.join(path, c))):
+            if(child in excluding or os.path.splitext(child)[1] in excluding): continue
+            childPath = os.path.join(path, child)
+            if(os.path.isdir(childPath)):
+                childNode = ET.SubElement(node, "folder")
+                childNode.attrib["name"] = child
+                walk(root, childNode, childPath)
+            else:
+                fileName, fileType = os.path.splitext(child)
+                childNode = ET.SubElement(node, "file")
+                childNode.attrib["updated"] = root.attrib["version"]
+                childNode.attrib["name"] = fileName
+                childNode.attrib["type"] = fileType[1:]
+                childNode.attrib["path"] = os.path.split(os.path.relpath(childPath, target))[0]
+    walk(root, root, target)
+
+    with open("struct.xml", "w") as f:
+        f.write(xml.dom.minidom.parseString(ET.tostring(root, xml_declaration=False)).toprettyxml(indent="\t"))
